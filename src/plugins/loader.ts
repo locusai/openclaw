48c8ae08f919722f77f4e7a7c31018c32aa41c5a
Rohit Amarnath <88762+ramarnat@users.noreply.github.com>
2026-02-21 17:56:04 -0500

    resolve conflicts
    (cherry picked from commit cdc4278ee55c33b9acbc26e2e010b8db0392ce76)

--- src/plugins/loader.ts
+++ src/plugins/loader.ts
@@ -168,6 +168,7 @@ function createPluginRecord(params: {
     cliCommands: [],
     services: [],
     commands: [],
+    commandOptions: [],
     httpHandlers: 0,
     hookCount: 0,
     configSchema: params.configSchema,
