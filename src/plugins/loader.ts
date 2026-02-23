2a543faac5dd75f39e8ce7a2ed4d45699d105d0c
Rohit Amarnath <88762+ramarnat@users.noreply.github.com>
2026-02-23 17:51:39 -0500

    fix(plugins): drop stale commandOptions fields
    (cherry picked from commit 32bcbfc3574f1132b8dff771a48aba9ea652c479)

--- src/plugins/loader.ts
+++ src/plugins/loader.ts
@@ -166,7 +166,6 @@ function createPluginRecord(params: {
     cliCommands: [],
     services: [],
     commands: [],
-    commandOptions: [],
     httpHandlers: 0,
     hookCount: 0,
     configSchema: params.configSchema,
